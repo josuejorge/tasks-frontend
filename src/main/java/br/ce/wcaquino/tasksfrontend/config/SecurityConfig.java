a31dd5cf2372ace7a620c1edbc64b91cda8da9ed
jorgeoliveira2099 <josuejorge2099@hotmail.com>
2026-06-05 21:06:41 -0300

    atualizado secutiry config

--- src/main/java/br/ce/wcaquino/tasksfrontend/config/SecurityConfig.java
+++ src/main/java/br/ce/wcaquino/tasksfrontend/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package br.ce.wcaquino.tasksfrontend.config;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,6 +10,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 @Configuration
@@ -20,6 +24,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Value("${app.base-url:http://localhost:9999}")
 	private String appBaseUrl;
+
+	@Value("${spring.security.oauth2.client.registration.keycloak.client-id}")
+	private String clientId;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -42,12 +49,33 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 
 	private LogoutSuccessHandler keycloakLogoutSuccessHandler() {
-		// Deriva a URL base do Keycloak a partir da authorization-uri já configurada
-		// ex: .../protocol/openid-connect/auth -> .../protocol/openid-connect/logout
+		// Keycloak 18+ exige client_id ou id_token_hint junto com post_logout_redirect_uri
 		return (HttpServletRequest request, HttpServletResponse response, Authentication authentication) -> {
 			String logoutEndpoint = keycloakAuthUri.replace("/auth", "/logout");
-			String redirectUri = appBaseUrl + "/tasks/";
-			response.sendRedirect(logoutEndpoint + "?redirect_uri=" + redirectUri);
+			String redirectUri = encode(appBaseUrl + "/tasks/");
+
+			StringBuilder url = new StringBuilder(logoutEndpoint)
+				.append("?client_id=").append(clientId)
+				.append("&post_logout_redirect_uri=").append(redirectUri);
+
+			// inclui id_token_hint para garantir que o Keycloak aceite o redirect
+			if (authentication instanceof OAuth2AuthenticationToken) {
+				OAuth2AuthenticationToken token = (OAuth2AuthenticationToken) authentication;
+				if (token.getPrincipal() instanceof OidcUser) {
+					String idToken = ((OidcUser) token.getPrincipal()).getIdToken().getTokenValue();
+					url.append("&id_token_hint=").append(idToken);
+				}
+			}
+
+			response.sendRedirect(url.toString());
 		};
+	}
+
+	private String encode(String value) {
+		try {
+			return URLEncoder.encode(value, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			return value;
+		}
 	}
 }
